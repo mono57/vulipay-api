a2122eecb0151d12ae74828ea02cb479d72eab70
Amono Aymar <aymar.amono@phoenix-gs.xyz>
2022-01-28 21:50:50 +0100

    add currency model

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
@@ -6,12 +8,12 @@ from django.utils import timezone
 
 from app.utils.timestamp import TimestampModel
 from app.utils.generate_code import generate_code
+from app.utils.twilio_client import MessageClient
 from accounts.managers import (
     PhoneNumberConfirmationCodeManager,
     AvailableCountryManager,
     UserManager)
 
-import datetime
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -42,18 +44,37 @@ class AvailableCountry(TimestampModel):
     def __str__(self):
         return f'({self.calling_code}) - {self.name} - {self.iso_code}'
 
+
+
+class Currency(TimestampModel):
+    iso_code = models.CharField(max_length=8)
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=5)
+    country = models.ForeignKey(
+        AvailableCountry,
+        null=True,
+        on_delete=models.SET_NULL
+    )
+
+    def __str__(self):
+        return '{} - {} - {}'.format(
+            self.name, self.iso_code, self.symbol
+        )
+
 class NetworkProvider(TimestampModel):
     name = models.CharField(max_length=30, verbose_name=_('name'))
 
     def __str__(self):
         return self.name
 
+
 class PhoneNumber(TimestampModel):
     number = models.CharField(max_length=20)
     primary = models.BooleanField(default=False)
     verified = models.BooleanField(default=False)
-    provider = models.ForeignKey(NetworkProvider, blank=True, null=True, on_delete=models.SET_NULL)
-
+    provider = models.ForeignKey(
+        NetworkProvider, blank=True, null=True, on_delete=models.SET_NULL
+    )
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -88,10 +109,16 @@ class PhoneNumberConfirmationCode(TimestampModel):
         return expiration_date <= timezone.now()
 
     @classmethod
-    def create(cls, phone_number):
-        key = generate_code(__class__)
-        return cls._default_manager.create(phone_number=phone_number, key = key)
+    def create(cls, int_phone_number):
+        klass = __class__
+        # klass.objects.force_expired()
+        key = generate_code(klass)
+        return cls._default_manager.create(phone_number=int_phone_number, key = key)
 
     def send_key(self):
-        # Send confirmation code here
-        print("Code sent", self.key)
+        body = MessageClient._BODY_VIRIFICATION.format(self.key)
+        sid = MessageClient.send_message(
+            body, self.phone_number
+        )
+
+        self.sent = datetime.datetime.now()
