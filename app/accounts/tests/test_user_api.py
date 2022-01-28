826518d4bdfaad7597f982db4ccb51ad790b8660
Amono Aymar <aymar.amono@phoenix-gs.xyz>
2022-01-28 21:26:39 +0100

    test registration endpoint

--- app/accounts/tests/test_user_api.py
+++ app/accounts/tests/test_user_api.py
@@ -10,17 +10,25 @@ from accounts.tests.factories import AvailableCountryFactory
 User = get_user_model()
 
 CREATE_USER_URL = reverse('accounts:register')
+RESEND_CONFIRM_CODE_URL = reverse('accounts:resend-code')
 CONFIRM_CODE_URL = reverse('accounts:confirm')
 
 class TestPublicUserApi(TestCase):
     def setUp(self):
         self.client = APIClient()
+
+        self.registration_payload = {
+            'phone_number': '698049742',
+            'country_iso_code': 'CM'
+        }
+
         self.country_payload = {
             'name': 'Cameroun',
             'calling_code': '237',
             'phone_number_regex': '^6[5-9][0-9]{7}$',
             'iso_code': 'CM'
         }
+
         AvailableCountry.objects.create(**self.country_payload)
 
     def test_country_available(self):
@@ -30,13 +38,12 @@ class TestPublicUserApi(TestCase):
 
 
     def test_create_inactive_user(self):
-        payload = {
-            'phone_number': '698049742',
-            'country_iso_code': 'CM'
-        }
-        response = self.client.post(CREATE_USER_URL, payload)
+        response = self.client.post(CREATE_USER_URL, self.registration_payload)
         self.assertTrue(response.status_code == status.HTTP_201_CREATED)
 
+    def test_resend_user_confirmation_code(self):
+        response = self.client.post(RESEND_CONFIRM_CODE_URL, self.registration_payload)
+        self.assertTrue(response.status_code == status.HTTP_200_OK)
 
     def test_can_confirm_code(self):
         pass
