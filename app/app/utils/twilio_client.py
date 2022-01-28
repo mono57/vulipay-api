12b2a1b6270dafe62367ddf47df9381125bc7c1b
Amono Aymar <aymar.amono@phoenix-gs.xyz>
2022-01-28 21:30:13 +0100

    refactor twilio client

--- app/app/utils/twilio_client.py
+++ app/app/utils/twilio_client.py
@@ -3,16 +3,20 @@ from twilio.rest import Client
 import os
 
 class MessageClient:
-    def __init__(self):
+    _BODY_VIRIFICATION = "Your vefirification code is {0}"
+
+    @classmethod
+    def send_message(cls, body, to):
         account_ssid = os.environ.get('TWILIO_ACCOUNT_SSID')
         auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
-        self.sender = os.environ.get('TWILIO_SENDER')
-        print('account_ssid', account_ssid)
-        self.twilio_client = Client(account_ssid, auth_token)
+        sender = os.environ.get('TWILIO_SENDER')
 
-    def send_message(self, body, to):
-        self.twilio_client.messages.create(
+        twilio_client = Client(account_ssid, auth_token)
+
+        message = twilio_client.messages.create(
             body=body,
             to=to,
-            from_=self.sender,
+            from_=sender,
         )
+
+        return message.sid
