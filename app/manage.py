f1ac1a7587d7925d827dd552f2b869eff481ccf5
Amono Aymar <aymar.amono@phoenix-gs.xyz>
2022-01-30 19:13:15 +0100

    refactor(database): add pgsql engine

--- app/manage.py
+++ app/manage.py
@@ -2,13 +2,15 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
+
 import dotenv
 
 
 def main():
     """Run administrative tasks."""
-    dotenv.read_dotenv()
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+    dotenv.read_dotenv(os.path.join(Path(__file__).resolve().parent.parent, ".env"))
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -20,5 +22,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
