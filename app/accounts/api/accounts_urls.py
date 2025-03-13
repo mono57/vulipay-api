2cfd222ef1a4affa892011aad970d09053d3ad18
Aymar Amono <aymar.amono@vulipay.com>
2025-03-13 18:12:51 +0100

    feat: add token refresh endpoint to accounts API

--- app/accounts/api/accounts_urls.py
+++ app/accounts/api/accounts_urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from app.accounts.api.views import (
     AccountBalanceRetrieveAPIView,
@@ -44,4 +45,5 @@ urlpatterns = [
     path(
         "user/full-name", UserFullNameUpdateView.as_view(), name="user_full_name_update"
     ),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
