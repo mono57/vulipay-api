5eeaa247be8260fc2a7b453b289df62e8049b301
Aymar Amono <aymar.amono@vulipay.com>
2025-03-13 23:40:09 +0100

    chore: remove unused code

--- app/transactions/api/transactions_urls.py
+++ app/transactions/api/transactions_urls.py
@@ -3,41 +3,14 @@ from django.urls import path
 from app.transactions.api.views import (
     AddFundsCallbackAPIView,
     AddFundsTransactionCreateAPIView,
-    CashInTransactionCreateAPIView,
-    CashOutTransactionCreateAPIView,
-    MPTransactionCreateAPIView,
-    P2PTransactionCreateAPIView,
     PaymentMethodDetailAPIView,
     PaymentMethodListCreateAPIView,
     PaymentMethodTypeListAPIView,
-    TransactionDetailsRetrieveAPIView,
-    TransactionPairingUpdateAPIView,
-    ValidateTransactionUpdateAPIView,
 )
 
 app_name = "transactions"
 
 urlpatterns = [
-    path(
-        "P2P",
-        P2PTransactionCreateAPIView.as_view(),
-        name="transactions_p2p_transactions",
-    ),
-    path(
-        "MP",
-        MPTransactionCreateAPIView.as_view(),
-        name="transactions_mp_transactions",
-    ),
-    path(
-        "CO",
-        CashOutTransactionCreateAPIView.as_view(),
-        name="transactions_co_transactions",
-    ),
-    path(
-        "CI",
-        CashInTransactionCreateAPIView.as_view(),
-        name="transactions_ci_transactions",
-    ),
     path(
         "cash-in",
         AddFundsTransactionCreateAPIView.as_view(),
@@ -47,21 +20,6 @@ urlpatterns = [
         "cash-in/callback",
         AddFundsCallbackAPIView.as_view(),
         name="transactions_cash_in_callback",
-    ),
-    path(
-        "<str:payment_code>/details",
-        TransactionDetailsRetrieveAPIView.as_view(),
-        name="transactions_transaction_details",
-    ),
-    path(
-        "<str:reference>/validate",
-        ValidateTransactionUpdateAPIView.as_view(),
-        name="transactions_transaction_validate",
-    ),
-    path(
-        "<str:payment_code>/pairing",
-        TransactionPairingUpdateAPIView.as_view(),
-        name="transactions_transaction_pairing",
     ),
     path(
         "payment-methods/",
