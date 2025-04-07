794d1630fe7cd94ed5cc12af3cfd824468ec99e6
Aymar Amono <aymar.amono@vulipay.com>
2025-04-07 18:04:05 +0100

    refactor: remove custom JWT authentication class and update settings to use default authentication

--- config/settings/base.py
+++ config/settings/base.py
@@ -199,9 +199,6 @@ LOGGING = {
 # DJANGO REST FRAMEWORK
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "app.accounts.authentication.AppJWTAuthentication",
-    ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
